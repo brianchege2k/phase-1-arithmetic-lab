747909d297b94ad38afb55ecf217170c6eff2ac5
brianchege2k <chegebrian84@gmail.com>
2024-03-20 15:31:47 +0300

    arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+const num1= 31
+const num2 =2
+
+const multiply = num1 * num2
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+console.log(random)  
+
+const num3 = 34
+const num4 = 5
+const mod = num3 % num4
+console.log(mod)
+
+
+const max = Math.max(1,2,4,20)
+
+console.log(max)
+
+
